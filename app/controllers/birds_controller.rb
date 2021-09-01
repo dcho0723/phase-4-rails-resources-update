54285e8c060cbb1def0335651e8aa22ec8e11814
dcho0723 <dcho0723@yahoo.com>
2021-09-01 00:55:43 -0400

    finsihed

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -12,6 +12,12 @@ class BirdsController < ApplicationController
     render json: bird, status: :created
   end
 
+  def update
+    bird = Bird.find_by(id: params[:id])
+    bird.update(bird_params)
+    render json: bird, status: :accepted
+  end
+
   # GET /birds/:id
   def show
     bird = Bird.find_by(id: params[:id])
@@ -22,10 +28,20 @@ class BirdsController < ApplicationController
     end
   end
 
+  def increment_likes
+    bird = Bird.find_by(id: params[:id])
+    if bird
+      bird.update(likes: bird.likes + 1)
+      render json: bird
+    else
+      render json: { error: "bird not found" }, status: :not_found
+    end
+  end
+
   private
 
   def bird_params
-    params.permit(:name, :species)
+    params.permit(:name, :species, :likes)
   end
 
 end
